9f0f2e2c13e23b52ea33e61e6b9ca6173d163e4c
Sam <19FTT1067@student.edu.bn>
2021-02-10 09:43:43 +0800

    Lesson 1.3 - Creating a Window

---
+++ project01/WizardOfYesNo.java
@@ -0,0 +1,19 @@
+package project01;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WizardOfYesNo extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		arg0.show();
+		
+	}
+	
+	public static void main(String [] args) {
+		Application.launch(args);
+		
+	}
+
+}
