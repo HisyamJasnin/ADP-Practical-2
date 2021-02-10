7fa7516f34611f97a2a91d31bbbdc4934cc6429d
Sam <19FTT1067@student.edu.bn>
2021-02-10 15:28:53 +0800

    Lesson 2.6 - Buttons

--- project02/GuessMyColour.java
+++ project02/GuessMyColour.java
@@ -3,13 +3,17 @@ package project02;
 import java.util.Random;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.*;
@@ -37,6 +41,10 @@ public class GuessMyColour extends Application {
 	private int targetGreen = 0;
 	private int targetBlue = 0;
 	
+	private int red = 0;
+	private int green = 0;
+	private int blue = 0;
+
 	public void initGUI(Stage arg0) {
 		Label titleLabel = new Label();
 		Font titleFont = Font.font("Serif", FontWeight.BOLD, 32);
@@ -64,9 +72,132 @@ public class GuessMyColour extends Application {
 		rootPane.setCenter(centerPane);
 		
 		generateTargetColour();
-
+		
+		HBox buttonPane = new HBox();
+		
+		Font font = Font.font("Dialog", FontWeight.BOLD, 18);
+		
+		Button moreRedButton = new Button("+");
+		moreRedButton.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
+		moreRedButton.setFont(font);
+		
+		Button lessRedButton = new Button("-");
+		lessRedButton.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
+		lessRedButton.setFont(font);
+	
+		Button moreGreenButton = new Button("+");
+		moreGreenButton.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
+		moreGreenButton.setFont(font);
+		
+		Button lessGreenButton = new Button("-");
+		lessGreenButton.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
+		lessGreenButton.setFont(font);
+	
+		
+		Button moreBlueButton = new Button("+");
+		moreBlueButton.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
+		moreBlueButton.setFont(font);
+		
+		Button lessBlueButton = new Button("-");
+		lessBlueButton.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
+		lessBlueButton.setFont(font);
+		
+		
+		moreRedButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				increaseRed();	
+			}
+		});
+		
+		lessRedButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				decreaseRed();	
+			}
+		});
+		
+		moreGreenButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				increaseGreen();	
+			}
+		});
+		
+		lessGreenButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				decreaseGreen();	
+			}
+		});
+		
+		moreBlueButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				increaseBlue();	
+			}
+		});
+		
+		lessBlueButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				decreaseBlue();	
+			}
+		});
+		
+		buttonPane.getChildren().addAll(moreRedButton,lessRedButton,moreGreenButton,lessGreenButton,moreBlueButton,lessBlueButton);
+		
+		rootPane.setBottom(buttonPane);
+		
 		Scene root = new Scene(rootPane);
 		arg0.setScene(root);
+	}
+	
+	private void increaseRed() {
+		if(red <= 240) {
+			red = red +15;
+			updateColourSample();
+		}
+	}
+	
+	private void decreaseRed() {
+		if(red >= 15) {
+			red = red - 15;
+			updateColourSample();
+		}
+	}
+	
+	private void increaseGreen() {
+		if(green <=240) {
+			green = green +15;
+			updateColourSample();
+		}
+	}
+	
+	private void decreaseGreen() {
+		if(green >= 15) {
+			green = green - 15;
+			updateColourSample();
+		}
+	}
+	
+	private void increaseBlue() {
+		if(blue <=240) {
+			blue = blue +15;
+			updateColourSample();
+		}
+	}
+	
+	private void decreaseBlue() {
+		if(blue >= 15) {
+			blue = blue - 15;
+			updateColourSample();
+		}
+	}
+	
+	private void updateColourSample() {
+		Color color = Color.rgb(red, green, blue);
+		sampleRectangle.setFill(color);
 	}
 	
 	private void generateTargetColour() {
