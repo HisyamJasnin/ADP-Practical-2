3e1aed93c78a890d9cc1589d3172830d57996776
Sam <19FTT1067@student.edu.bn>
2021-02-10 13:46:29 +0800

    Lesson 2.2 - BorderPane Layout

--- project02/GuessMyColour.java
+++ project02/GuessMyColour.java
@@ -1,18 +1,42 @@
 package project02;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 public class GuessMyColour extends Application {
+	
+	
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+		initGUI(arg0);
+		
 		arg0.setTitle("Guess My Colour");
 		arg0.setResizable(false);
 		arg0.sizeToScene();
 		arg0.show();
 		
 	}
+	
+	public void initGUI(Stage arg0) {
+		Label titleLabel = new Label();
+		Font titleFont = Font.font("Serif", FontWeight.BOLD, 32);
+		titleLabel.setFont(titleFont);
+		titleLabel.setAlignment(Pos.CENTER);
+		titleLabel.setText("Guess My Colour");
+		BorderPane rootPane = new BorderPane();
+		rootPane.setTop(titleLabel);
+		
+		Scene root = new Scene(rootPane);
+		arg0.setScene(root);
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		Application.launch(args);
