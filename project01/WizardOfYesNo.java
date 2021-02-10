0ee1b874fe1fd17354ed3f8b2e36788e4e4f7bc4
Sam <19FTT1067@student.edu.bn>
2021-02-10 09:50:48 +0800

    Lesson 1.4 - Add Text, Window Size

--- project01/WizardOfYesNo.java
+++ project01/WizardOfYesNo.java
@@ -1,12 +1,24 @@
 package project01;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class WizardOfYesNo extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+		// Create the Label object
+		Label label = new Label("Hello World");
+		
+		// Create the Scene object and add Label object into it.
+		// Add width and height for window size
+		Scene root = new Scene(label, 200, 100);
+		
+		//Set Stage to use the Scene object
+		arg0.setScene(root);
+		
 		arg0.show();
 		
 	}
